10bdbacf6902f9b06b77c6ba8c2bcf3b06f9e788
Harshvyas2704 <harshvyasdev@gmail.com>
2023-09-03 16:16:17 +0530

    episod2 notes and code

---
+++ class_notes/inception/README.md
@@ -0,0 +1,20 @@
+# NAMASTE REACT
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR (Hot Module Replacement)
+- File Waching Algorithm -> written in c++
+- Caching - Faseter Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Constistent Hashing
+- Code Spliting
+- Differential Building
+- Diagonstic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different build for dev and production
--- class_notes/inception/app.js
+++ class_notes/inception/app.js
@@ -1,6 +1,13 @@
-/**
+import React from "react"
+import ReactDOM from "react-dom/client";
+
+/** FEEL THIS PAIN
  * <div id="parent">
  *     <div id="child">
+ *         <h1>I am h1 tag</h1>
+ *         <h2>I am h2 tag</h2>
+ *     </div>
+ *    <div id="child1">
  *         <h1>I am h1 tag</h1>
  *         <h2>I am h2 tag</h2>
  *     </div>
@@ -9,10 +16,10 @@
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child}" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h1 tag"),
+    React.createElement("h1", {}, "Namaste React"),
+    React.createElement("h2", {}, "Harsh Vyas"),
   ]),
-  React.createElement("div", { id: "child2}" }, [
+  React.createElement("div", { id: "child1}" }, [
     React.createElement("h1", {}, "I am h1 tag"),
     React.createElement("h2", {}, "I am h1 tag"),
   ]),
